e631b22c2f29f73765cea027b567c2f33c118ece
Marc Fargas <marc@marcfargas.com>
2026-03-28 16:51:09 +0100

    feat: include commit hash in bifrost version output

--- cmd/bifrost/version.go
+++ cmd/bifrost/version.go
@@ -7,12 +7,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var version = "dev" // set via ldflags: -ldflags "-X main.version=x.y.z"
+var (
+	version = "dev"     // set via ldflags: -ldflags "-X main.version=x.y.z"
+	commit  = "unknown" // set via ldflags: -ldflags "-X main.commit=abc1234"
+)
 
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Print version and protocol version",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Printf("bifrost %s (protocol %s)\n", version, protocol.ProtocolVersion)
+		fmt.Printf("bifrost %s (%s) protocol %s\n", version, commit, protocol.ProtocolVersion)
 	},
 }
